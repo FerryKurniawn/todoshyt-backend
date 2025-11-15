54a6b49c44b07b0d6c1b365bee806d6fa4a25d3f
FerryKurniawn <itqan.fk@gmail.com>
2025-11-15 16:30:32 +0700

    feat: add user model prisma

---
+++ prisma/migrations/20251115092916_add/migration.sql
@@ -0,0 +1,12 @@
+-- AlterTable
+ALTER TABLE "Task" ADD COLUMN     "userId" TEXT;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
